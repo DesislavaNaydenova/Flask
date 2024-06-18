2735ea8331655d0523df90fcb7245b0fc78378d4
Desislava Naydenova <deskata.01@gmail.com>
2024-06-18 06:50:59 +0000

    Add a flash message

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -34,7 +35,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print (request.form)
+        flash("Thanks!{}, we have recieved your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
